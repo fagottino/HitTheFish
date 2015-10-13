a4405aa3b3915a0e518bfcf6e9078f6dfffc48fd
Admin <Admin@Admin-HP>
2015-10-14 00:06:32 +0200

    resolved background

--- src/hitthefish/GUI/Game.java
+++ src/hitthefish/GUI/Game.java
@@ -21,25 +21,15 @@ public class Game extends JPanel {
     private final BufferedImage gun;
     public Thread wavesThread;
     public String name;
-    private int timer;
-    private int conta;
-    private String[] array;
-    private int i;
-            
+    private int timer, i;
+    
     public Game() {
         this.setSize(HitTheFish.FRAME_SIZE);
         background = Resources.getImage("../img/bg.png");
         gun = Resources.getImage("../img/gun.png");
         wavesThread = new Thread(new wavesMove());
         bg = Resources.getImage("../img/bg1.png");
-        conta = 0;
-        array = new String[5];
-        array[0] = "bg1.png";
-        array[1] = "bg2.png";
-        array[2] = "bg3.png";
-        array[3] = "bg4.png";
-        array[4] = "bg5.png";
-        i = 0;
+        timer = 200;
     }
     
     @Override
@@ -51,25 +41,15 @@ public class Game extends JPanel {
     public void update(Graphics g) {
         
         g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+        i = 1 + (int)(Math.random()*5);
+        sfondo(g, i);
         g.drawImage(gun, getWidth() - 350, getHeight() - 152, this);
-        conta++;
-        sfondo(g);
     }
     
-    private void sfondo(Graphics g){
-        if(conta == 5) {
-            name = array[i];
-            bg = Resources.getImage(name);
-            g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
-            conta = 0;
-            i++;
-        }
-        
-//        for(i = 1; i <= 5; i++) {
-//            name = "../img/bg" + i + ".png";
-//            bg = Resources.getImage(name);
-//            g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
-//        }
+    private void sfondo(Graphics g, int i) {
+        name = "../img/bg" + i + ".png";
+        bg = Resources.getImage(name);
+        g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
     }
     
     public class wavesMove implements Runnable {
